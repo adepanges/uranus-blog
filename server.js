e238474d236e9a370bd6626e4820ff540cc91ad8
Ade Pangestu <adepanges@gmail.com>
2018-10-02 22:02:03 +0700

    commit all

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+global.BASE_PATH = __dirname;
+global.APP_PATH = `${__dirname}/app`;
+global.PORT = 3000;
+
+require('./init')(app)
+loadApp('routes')(app)
+
+
+app.listen(PORT, () => {
+    logger.debug('Server is listening on port 3000');
+});
